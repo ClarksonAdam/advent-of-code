6df722a9730ef502a693d60d3dc12c445f5454e4
adamclarkson <adam.clarkson1@nhs.net>
2024-12-02 15:17:33 +0000

    fix: better tests for day 2

--- 2024/day-2/day_2.py
+++ 2024/day-2/day_2.py
@@ -12,16 +12,20 @@ def check_is_report_safe(report):
     # Check that the difference between numbers is correct
     return all(0 < abs(report[l] - report[l + 1]) < 4 for l in range(len(report) - 1))
 
+def count_safe_and_unsafe_reports(reports):
+    safe_reports = 0
+    not_safe_reports = 0
+    for report in reports:
+        if check_is_report_safe(report):
+            safe_reports += 1
+        else:
+            not_safe_reports += 1
+    return safe_reports, not_safe_reports
 
 reports = extract_reports_from_file("day_2_input.txt")
 
 # Task 1
-safe_reports = 0
-not_safe_reports = 0
-for report in reports:
-    if check_is_report_safe(report): safe_reports += 1
-    else: not_safe_reports += 1
-
+safe_reports, not_safe_reports = count_safe_and_unsafe_reports(reports)
 print(f"Task 1. Safe reports: {safe_reports}. "
       f"Not safe reports: {not_safe_reports}. "
       f"Total: {safe_reports + not_safe_reports}")
--- 2024/day-2/test_day_2.py
+++ 2024/day-2/test_day_2.py
@@ -2,7 +2,6 @@ import pytest
 
 import day_2
 
-
 def test_extract_reports_from_file():
     reports = day_2.extract_reports_from_file("day_2_input.txt")
     assert reports[0] == [74, 76, 78, 79, 76]
@@ -14,3 +13,8 @@ def test_extract_reports_from_file():
                          [([1, 3, 5, 8, 9], True), ([1, 3, 2, 9, 10], False), ([1, 3, 5, 7, 7], False)])
 def test_check_is_report_safe(input, expected):
     assert day_2.check_is_report_safe(input) == expected
+
+
+def test_count_safe_and_unsafe_reports():
+    reports = [[1, 3, 5, 8, 9], [1005, 1007, 1010, 1011, 1012], [1, 3, 2, 9, 10], [10, 13, 15, 17, 17]]
+    assert day_2.count_safe_and_unsafe_reports(reports) == (2, 2)
