4161fdf1006ad238c0f38ed31aaad6d874417dbd
adamclarkson <adam.clarkson1@nhs.net>
2024-12-03 16:20:19 +0000

    feat: day 2 task 2 and refactor of task 1

--- 2024/day-2/day_2.py
+++ 2024/day-2/day_2.py
@@ -3,29 +3,61 @@ def extract_reports_from_file(filepath):
         return [[int(level) for level in line.strip().split(" ")] for line in file]
 
 
-def check_is_report_safe(report):
-    #check that the arrays are ascending or descending
+def is_ascending_or_descending(report):
     is_ascending = all(report[i] < report[i + 1] for i in range(len(report) - 1))
     is_descending = all(report[i] > report[i + 1] for i in range(len(report) - 1))
     if not (is_ascending or is_descending): return False
+    else: return True
 
-    # Check that the difference between numbers is correct
+
+def has_valid_difference(report):
     return all(0 < abs(report[l] - report[l + 1]) < 4 for l in range(len(report) - 1))
 
-def count_safe_and_unsafe_reports(reports):
-    safe_reports = 0
-    not_safe_reports = 0
+
+def is_report_safe(report):
+    return is_ascending_or_descending(report) and has_valid_difference(report)
+
+
+def evaluate_reports(reports):
+    safe, unsafe = 0, 0
     for report in reports:
-        if check_is_report_safe(report):
+        if is_report_safe(report):
+            safe += 1
+        else:
+            unsafe += 1
+    return safe, unsafe
+
+
+def evaluate_reports_with_removal(reports):
+    safe_reports, unsafe_reports = 0, 0
+    for report in reports:
+        if is_report_safe(report):
             safe_reports += 1
         else:
-            not_safe_reports += 1
-    return safe_reports, not_safe_reports
+            safe_levels, unsafe_levels = 0, 0
+            for i in range(len(report)):
+                temp_report = report.copy()
+                del temp_report[i]
+                if is_report_safe(temp_report):
+                    safe_levels += 1
+                else:
+                    unsafe_levels += 1
+            if safe_levels == 0: unsafe_reports += 1
+            elif safe_levels >= 1: safe_reports += 1
+
+    return safe_reports, unsafe_reports
+
 
 reports = extract_reports_from_file("day_2_input.txt")
 
 # Task 1
-safe_reports, not_safe_reports = count_safe_and_unsafe_reports(reports)
-print(f"Task 1. Safe reports: {safe_reports}. "
-      f"Not safe reports: {not_safe_reports}. "
-      f"Total: {safe_reports + not_safe_reports}")
+safe_reports_task_1, unsafe_reports_task_1 = evaluate_reports(reports)
+print(f"Task 1. Safe reports: {safe_reports_task_1}. "
+      f"Unsafe reports: {unsafe_reports_task_1}. "
+      f"Total: {safe_reports_task_1 + unsafe_reports_task_1}")
+
+# Task 2
+safe_reports_task_2, unsafe_reports_task_2 = evaluate_reports_with_removal(reports)
+print(f"Task 2. Safe reports: {safe_reports_task_2}. "
+      f"Unsafe reports: {unsafe_reports_task_2}. "
+      f"Total: {safe_reports_task_2 + unsafe_reports_task_2}")
--- 2024/day-2/test_day_2.py
+++ 2024/day-2/test_day_2.py
@@ -11,10 +11,27 @@ def test_extract_reports_from_file():
 
 @pytest.mark.parametrize('input,expected',
                          [([1, 3, 5, 8, 9], True), ([1, 3, 2, 9, 10], False), ([1, 3, 5, 7, 7], False)])
-def test_check_is_report_safe(input, expected):
-    assert day_2.check_is_report_safe(input) == expected
+def test_is_ascending_or_descending(input, expected):
+    assert day_2.is_ascending_or_descending(input) is expected
 
 
-def test_count_safe_and_unsafe_reports():
+@pytest.mark.parametrize('input,expected',
+                         [([1005, 1007, 1010, 1011, 1012], True), ([10, 13, 17, 18, 19], False)])
+def test_has_valid_difference(input, expected):
+    assert day_2.has_valid_difference(input) is expected
+
+
+@pytest.mark.parametrize('input,expected',
+                         [([105, 107, 110, 111, 111], False), ([10, 13, 17, 18, 19], False), ([1, 5, 4, 5, 7], False), ([1, 4, 5, 8, 9], True)])
+def test_is_report_safe(input, expected):
+    assert day_2.has_valid_difference(input) is expected
+
+
+def test_evaluate_reports():
     reports = [[1, 3, 5, 8, 9], [1005, 1007, 1010, 1011, 1012], [1, 3, 2, 9, 10], [10, 13, 15, 17, 17]]
-    assert day_2.count_safe_and_unsafe_reports(reports) == (2, 2)
+    assert day_2.evaluate_reports(reports) == (2, 2)
+
+
+def test_evaluate_reports_with_removals():
+    reports = [[1, 3, 5, 8, 9], [1005, 1007, 1010, 1011, 1012], [1, 3, 6, 5, 7], [10, 13, 15, 17, 17]]
+    assert day_2.evaluate_reports_with_removal(reports) == (4, 0)
